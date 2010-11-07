84da7cb6a99df241822817c2e8748e64640ae097
dinamo.forever@gmail.com <dinamo.forever@gmail.com@268e31a9-a219-ec89-1cb4-ecc417c412f6>
2010-11-07 18:01:27 +0000

    Edited ms_stdint.h so it can get compiled in VS2010

--- ghost/ms_stdint.h
+++ ghost/ms_stdint.h
@@ -66,7 +66,7 @@
 // 7.18.1 Integer types
 
 // 7.18.1.1 Exact-width integer types
-typedef __int8            int8_t;
+//typedef __int8            int8_t;
 typedef __int16           int16_t;
 typedef __int32           int32_t;
 typedef __int64           int64_t;
@@ -76,7 +76,7 @@ typedef unsigned __int32  uint32_t;
 typedef unsigned __int64  uint64_t;
 
 // 7.18.1.2 Minimum-width integer types
-typedef int8_t    int_least8_t;
+//typedef int8_t    int_least8_t;
 typedef int16_t   int_least16_t;
 typedef int32_t   int_least32_t;
 typedef int64_t   int_least64_t;
@@ -86,12 +86,12 @@ typedef uint32_t  uint_least32_t;
 typedef uint64_t  uint_least64_t;
 
 // 7.18.1.3 Fastest minimum-width integer types
-typedef int8_t    int_fast8_t;
-typedef int16_t   int_fast16_t;
+//typedef int8_t    int_fast8_t;
+//typedef int16_t   int_fast16_t;
 typedef int32_t   int_fast32_t;
 typedef int64_t   int_fast64_t;
 typedef uint8_t   uint_fast8_t;
-typedef uint16_t  uint_fast16_t;
+//typedef uint16_t  uint_fast16_t;
 typedef uint32_t  uint_fast32_t;
 typedef uint64_t  uint_fast64_t;
 
