c6ff4cc2ed4a075f8b64d7a6d6c184b972238f76
Joško Nikolić <josko.nikolic@fer.hr>
2013-11-28 01:28:48 +0100

    game: Rather than iterating through slots, iterate through fakeplayers in the outer loop in !deletefake

--- aura/game.cpp
+++ aura/game.cpp
@@ -4320,14 +4320,15 @@ void CGame::DeleteFakePlayers()
   if (m_FakePlayers.empty())
     return;
 
-  for (auto & slot : m_Slots)
+  for (auto & fakeplayer : m_FakePlayers)
   {
-    for (auto & fakeplayer : m_FakePlayers)
+    for (auto & slot : m_Slots)
     {
-      if (slot.GetPID() == fakeplayer)
+      if (slot.GetPID == fakeplayer)
       {
         slot = CGameSlot(0, 255, SLOTSTATUS_OPEN, 0, slot.GetTeam(), slot.GetColour(), slot.GetRace());
         SendAll(m_Protocol->SEND_W3GS_PLAYERLEAVE_OTHERS(fakeplayer, PLAYERLEAVE_LOBBY));
+        break;
       }
     }
   }
