Josko/aura-bot

commit 17b73a9342659292944a33ff58d41489b9d3fcb5

bnet: Only list appropriate file types in !map (.w3x and .w3m) and !load (.cfg)

aura/bnet.cpp
@@ -585,9 +585,6 @@ void CBNET::ProcessChatEvent(const CIncomingChatEvent *chatEvent)
             QueueChatCommand("The currently loaded map/config file is: [" + m_Aura->m_Map->GetCFGFile() + "]", User, Whisper, m_IRC);
           else
           {
-            string Pattern = Payload;
-            transform(Pattern.begin(), Pattern.end(), Pattern.begin(), ::tolower);
-
             if (!FileExists(m_Aura->m_MapPath))
             {
               Print("[BNET: " + m_ServerAlias + "] error listing maps - map path doesn't exist");
@@ -595,7 +592,7 @@ void CBNET::ProcessChatEvent(const CIncomingChatEvent *chatEvent)
             }
             else
             {
-              const vector<string> Matches = FilesMatch(m_Aura->m_MapPath, Pattern);
+              const vector<string> Matches = MapFilesMatch(Payload);
 
               if (Matches.empty())
                 QueueChatCommand("No maps found with that name", User, Whisper, m_IRC);
@@ -677,9 +674,6 @@ void CBNET::ProcessChatEvent(const CIncomingChatEvent *chatEvent)
             QueueChatCommand("The currently loaded map/config file is: [" + m_Aura->m_Map->GetCFGFile() + "]", User, Whisper, m_IRC);
           else
           {
-            string Pattern = Payload;
-            transform(Pattern.begin(), Pattern.end(), Pattern.begin(), ::tolower);
-
             if (!FileExists(m_Aura->m_MapCFGPath))
             {
               Print("[BNET: " + m_ServerAlias + "] error listing map configs - map config path doesn't exist");
@@ -687,7 +681,7 @@ void CBNET::ProcessChatEvent(const CIncomingChatEvent *chatEvent)
             }
             else
             {
-              const vector<string> Matches = FilesMatch(m_Aura->m_MapCFGPath, Pattern);
+              const vector<string> Matches = ConfigFilesMatch(Payload);
 
               if (Matches.empty())
                 QueueChatCommand("No map configs found with that name", User, Whisper, m_IRC);
@@ -1843,4 +1837,43 @@ void CBNET::HoldClan(CGame *game)
 {
   for (auto & clanmate : m_Clan)
     game->AddToReserved(clanmate);
+}
+
+vector<string> CBNET::MapFilesMatch(string pattern)
+{
+  transform(pattern.begin(), pattern.end(), pattern.begin(), ::tolower);
+
+  auto ROCMaps = FilesMatch(m_Aura->m_MapCFGPath, ".w3m");
+  auto TFTMaps = FilesMatch(m_Aura->m_MapCFGPath, ".w3x");
+
+  vector<string> MapList;
+  MapList.insert(end(MapList), begin(ROCMaps), end(ROCMaps));
+  MapList.insert(end(MapList), begin(TFTMaps), end(TFTMaps));
+
+  vector<string> Matches;
+
+  for (auto & mapName : MapList)
+  {
+    if (mapName.find(pattern) != string::npos)
+      Matches.push_back(mapName);
+  }
+
+  return Matches;
+}
+
+vector<string> CBNET::ConfigFilesMatch(string pattern)
+{
+  transform(pattern.begin(), pattern.end(), pattern.begin(), ::tolower);
+
+  vector<string> ConfigList = FilesMatch(m_Aura->m_MapCFGPath, ".cfg");
+
+  vector<string> Matches;
+
+  for (auto & cfgName : ConfigList)
+  {
+    if (cfgName.find(pattern) != string::npos)
+      Matches.push_back(cfgName);
+  }
+
+  return Matches;
 }

aura/bnet.h
@@ -135,6 +135,10 @@ public:
   CDBBan *IsBannedName(std::string name);
   void HoldFriends(CGame *game);
   void HoldClan(CGame *game);
+
+private:
+  std::vector<std::string> MapFilesMatch(std::string pattern);
+  std::vector<std::string> ConfigFilesMatch(std::string pattern);
 };
 
 #endif  // AURA_BNET_H_

commit 04a5800afdd08e5e10a3f2afb378457575b074e1

bnet: Fix a bug caused by commit 17b73a9

aura/bnet.cpp
@@ -1843,8 +1843,8 @@ vector<string> CBNET::MapFilesMatch(string pattern)
 {
   transform(pattern.begin(), pattern.end(), pattern.begin(), ::tolower);
 
-  auto ROCMaps = FilesMatch(m_Aura->m_MapCFGPath, ".w3m");
-  auto TFTMaps = FilesMatch(m_Aura->m_MapCFGPath, ".w3x");
+  auto ROCMaps = FilesMatch(m_Aura->m_MapPath, ".w3m");
+  auto TFTMaps = FilesMatch(m_Aura->m_MapPath, ".w3x");
 
   vector<string> MapList;
   MapList.insert(end(MapList), begin(ROCMaps), end(ROCMaps));