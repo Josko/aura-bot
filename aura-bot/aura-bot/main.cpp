5e037dbefed32514d596156ebf53ca04cd5bb1df
Julian <MyFakeAcc.4@googlemail.com>
2014-03-12 18:14:24 +0100

    Introducing: Xcode project files
    One project file for each dependency (stormlib and bncsutil) and one
    main project file (aura++.xcodeproj) containing the dependency projects.
    Please note: the project file in StormLib/ is not the one produced by
    cmake.

---
+++ aura-bot/aura-bot/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  aura-bot
+//
+//  Created by Julian on 12.03.14.
+//  Copyright (c) 2014 aura-bot. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
