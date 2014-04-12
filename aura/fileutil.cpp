Josko/aura-bot

commit 13a44eb26831db622a2842c32a6890f8ca5d4cec

fileutil: fix a resource leak where closedir wasn't called on Linux and CloseFind wasn't called on Windows (Coverity)

aura/fileutil.cpp
@@ -63,7 +63,10 @@ vector<string> FilesMatch(const string &path, const string &pattern)
     transform(begin(Name), end(Name), begin(Name), ::tolower);
 
     if (Name == pattern)
-      return vector<string> {Name};
+    {
+      Files.push_back(string(dp->d_name));
+      break;
+    }
 
     if (Name.find(pattern) != string::npos && Name != "..")
       Files.push_back(string(data.cFileName));
@@ -87,7 +90,10 @@ vector<string> FilesMatch(const string &path, const string &pattern)
     transform(begin(Name), end(Name), begin(Name), ::tolower);
 
     if (Name == pattern)
-      return vector<string> {Name};
+    {
+      Files.push_back(string(dp->d_name));
+      break;
+    }
 
     if (Name.find(pattern) != string::npos && Name != "." && Name != "..")
       Files.push_back(string(dp->d_name));

commit a5f9765a58bfea2a7b35a946f751193b3839f366

fileutil: fix a typo caused by commit 13a44eb268

aura/fileutil.cpp
@@ -64,7 +64,7 @@ vector<string> FilesMatch(const string &path, const string &pattern)
 
     if (Name == pattern)
     {
-      Files.push_back(string(dp->d_name));
+      Files.push_back(string(data.cFileName));
       break;
     }
 