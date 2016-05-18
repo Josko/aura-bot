Josko/aura-bot

commit d876a0a04a3d9659f496d1858beb7b3743cba375

gameplayer: Fix a potential nullptr dereference of m_Socket in CGamePlayer::Update() function.
In the case when m_Socket is indeed nullptr and m_Delete me is false,
then short circuit evaluation will not happen and therefore cause a
crash.

src/gameplayer.cpp
@@ -414,7 +414,10 @@ bool CGamePlayer::Update(void *fd)
   if (m_GProxy && m_Game->GetGameLoaded())
     return m_DeleteMe;
 
-  return m_DeleteMe || m_Socket->HasError() || !m_Socket->GetConnected();
+  if (m_Socket)
+    return m_Socket->HasError() || !m_Socket->GetConnected();
+
+  return m_DeleteMe;
 }
 
 void CGamePlayer::Send(const BYTEARRAY &data)

commit d9b11ea40344c4019b28a784cebda89b46c89226

gameplayer: Logic correction of a slight deviation caused by commit d876a0a.

src/gameplayer.cpp
@@ -414,10 +414,13 @@ bool CGamePlayer::Update(void *fd)
   if (m_GProxy && m_Game->GetGameLoaded())
     return m_DeleteMe;
 
+  if (m_DeleteMe)
+      return true;
+
   if (m_Socket)
     return m_Socket->HasError() || !m_Socket->GetConnected();
 
-  return m_DeleteMe;
+  return false;
 }
 
 void CGamePlayer::Send(const BYTEARRAY &data)